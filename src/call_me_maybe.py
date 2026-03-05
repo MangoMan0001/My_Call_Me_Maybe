f04bc6ca44190f4a5cd6c7080d41359736c03436
Ayumu Hirose <ayhirose@c1r7s2.42tokyo.jp>
2026-03-05 15:31:11 +0900

    prepare

---
+++ src/call_me_maybe.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+def call_me_maybe() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    pass
---
+++ src/main.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+from call_me_maybe import call_me_maybe
+
+def main() -> None:
+    call_me_maybe()
+
+if __name__ == "__main__":
+    main()
